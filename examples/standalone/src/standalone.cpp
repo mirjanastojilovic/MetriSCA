51bef9c0b2fcb154d45c346d83051398a16b5f54
Dorian Ros <dorian.ros@epfl.ch>
2022-03-06 11:38:33 +0100

    Add missing license header

--- examples/standalone/src/standalone.cpp
+++ examples/standalone/src/standalone.cpp
@@ -1,3 +1,10 @@
+/**
+ * MetriSCA - A side-channel analysis library
+ * Copyright 2021, School of Computer and Communication Sciences, EPFL.
+ *
+ * All rights reserved. Use of this source code is governed by a
+ * BSD-style license that can be found in the LICENSE.md file.
+ */
 
 #include <metrisca.hpp>
 
