846d8e06285cf33f86ce90995ccfcd92bb640cc6
BoyeGuillaume <guillaume.boye@epfl.ch>
2023-05-20 19:02:50 +0200

    pubfb

--- src/metrisca/metrics/rank_enumeration_metric.cpp
+++ src/metrisca/metrics/rank_enumeration_metric.cpp
@@ -19,14 +19,164 @@
 #include "metrisca/core/indicators.hpp"
 #include "metrisca/core/arg_list.hpp"
 #include "metrisca/core/parallel.hpp"
+#include "metrisca/core/lazy_function.hpp"
 
 #include <limits>
 #include <atomic>
 #include <unordered_set>
 #include <unordered_map>
 #include <set>
+#include <functional>
 #include <math.h>
 
+
+#define DOUBLE_INFINITY (std::numeric_limits<double>::infinity())
+
+typedef std::string PartialKey; // Use to partialKey each byte of the key
+
+struct EnumeratedElement
+{
+    double score;
+    PartialKey partialKey;
+
+    inline EnumeratedElement(double score, PartialKey partialKey)
+    : partialKey(partialKey), score(score)
+    { }
+
+    inline EnumeratedElement()
+    : EnumeratedElement(0, {})
+    { }
+};
+typedef std::function<bool(std::vector<EnumeratedElement>&, size_t)> LazyGeneratorFn;
+
+
+class Enumerator : public std::enable_shared_from_this<Enumerator>
+{
+public:
+    Enumerator(LazyGeneratorFn lazyRowGen, LazyGeneratorFn lazyColGen, size_t bufferSize) 
+    : m_LazyRowGen(std::move(lazyRowGen)),
+      m_LazyColGen(std::move(lazyColGen)),
+      m_ListOfCandidates({ 0 }),
+      m_BufferSize(bufferSize)
+    { }
+
+    /**
+     * @brief Enumerate next @p N elements 
+     */
+    bool next(std::vector<EnumeratedElement>& output, size_t N)
+    {
+        // For each of the entry
+        while (N-- != 0) {
+            // Find the best candidate
+            size_t bj = std::numeric_limits<size_t>::max();
+            double bestScore = -DOUBLE_INFINITY;
+
+            for (size_t j = 0; j != m_ListOfCandidates.size(); ++j) {
+                // Skip candidates for column already fully explored                
+                if (m_ListOfCandidates[j] == std::numeric_limits<size_t>::max()) continue;
+
+                // Compute the score of the current element
+                double score = m_Row[j].score + m_Col[m_ListOfCandidates[j]].score;
+
+                // If this is the best score so-far then use it as reference
+                if (score > bestScore) {
+                    bestScore = score;
+                    bj = j;
+                }
+            }
+
+            // No element where found, aborting the operation
+            if (bj == std::numeric_limits<size_t>::max()) {
+                return true;
+            }
+
+            // Generate the next enumerated element
+            output.emplace_back(
+                bestScore,
+                m_Row[bj].partialKey + m_Col[m_ListOfCandidates[bj]].partialKey
+            );
+
+            // Update the list of candidates
+            m_ListOfCandidates[bj]++;
+
+            if (m_ListOfCandidates[bj] == 0) {
+                METRISCA_ASSERT(bj == m_ListOfCandidates.size() - 1);
+
+                // Attempt to push the row
+                if (!m_RowMaxOut) {
+                    m_RowMaxOut = m_LazyRowGen(m_Row, m_BufferSize);
+                }
+
+                // Attempt to push a new candidate for the last row, if possible otherwise just skip this step
+                if (m_ListOfCandidates.size() < m_Row.size()) {
+                    m_ListOfCandidates.emplace_back(0); 
+                }
+            }
+
+            // Generating more of the column array if required
+            if (m_ListOfCandidates[bj] == m_Col.size() && !m_ColMaxOut) {
+                m_ColMaxOut = m_LazyColGen(m_Col, m_BufferSize);
+            }
+
+            // In the case where we cannot fetch more column, then simply mark this column as filled
+            if (m_ListOfCandidates[bj] == m_Col.size()) {
+                m_ListOfCandidates[bj] = std::numeric_limits<size_t>::max();
+            }
+        }
+
+        // Return false -> there are still keys that were not enumerated
+        return false;
+    }
+
+    LazyGeneratorFn asLazyGenerator()
+    {
+        std::shared_ptr<Enumerator> shared_ptr = shared_from_this();
+        return [shared_ptr](std::vector<EnumeratedElement>& output, size_t N) -> bool {
+            return shared_ptr->next(output, N);
+        };
+    }
+
+private:
+    std::vector<size_t> m_ListOfCandidates;
+    std::vector<EnumeratedElement> m_Row;
+    std::vector<EnumeratedElement> m_Col;
+    LazyGeneratorFn m_LazyRowGen;
+    LazyGeneratorFn m_LazyColGen;
+
+    bool m_RowMaxOut{false};
+    bool m_ColMaxOut{false};
+    size_t m_BufferSize;
+};
+
+static LazyGeneratorFn asLazyGenerator(const std::array<double, 256>& elements)
+{
+    std::shared_ptr<size_t> current_index = std::make_shared<size_t>(0);
+    return [&elements, current_index](std::vector<EnumeratedElement>& output, size_t N) -> bool {
+        size_t& cIdx = *current_index;
+        
+        while (N-- != 0) {
+            if (cIdx >= 256) {
+                return true;
+            }
+
+            output.emplace_back(elements[cIdx++], PartialKey({ (char) cIdx }));
+        }
+
+        return false;
+    };
+}
+
+static std::vector<LazyGeneratorFn> Combine(const std::vector<LazyGeneratorFn>& array)
+{
+    if (array.size() <= 1) return;
+
+    std::vector<LazyGeneratorFn> result;
+    for (size_t i = 0; i != (array.size() >> 1); i += 2) {
+        result.push_back(std::make_shared<Enumerator>(array[i], array[i+1], 8)->asLazyGenerator());
+    }
+
+    return result;
+}
 
 namespace metrisca {
 
@@ -54,6 +204,12 @@ namespace metrisca {
         auto key_span = m_Dataset->GetKey(0);
         m_Key.insert(m_Key.end(), key_span.begin(), key_span.end());
 
+        // Maximum key size
+        if (m_Key.size() > 256) {
+            METRISCA_ERROR("Maximum key size reached, risk of stack overflow");
+            return Error::UNSUPPORTED_OPERATION;
+        }
+
         // Finally return the success of the initialization of the current plugin
         return {};
     }
@@ -79,8 +235,21 @@ namespace metrisca {
         {
             // Retrieve the score corresponding with the current step
             const std::vector<std::array<double, 256>>& scores = scoresPerSteps[steps[idx]];
+        
+            // Constructing the enumerating context
+            std::vector<LazyGeneratorFn> lazyGenerators;
 
-            
+            for (size_t keyByte = 0; keyByte != scores.size(); ++keyByte) {
+                lazyGenerators.emplace_back(asLazyGenerator(scores[keyByte]));
+            }
+
+            while (lazyGenerators.size() > 1) {
+                lazyGenerators = Combine(lazyGenerators);
+            }
+
+            // Finally use the enumerator
+            std::vector<EnumeratedElement>& output;
+            lazyGenerators[0](result, m_EnumeratedKeyCount);
         });
 
         return {};
@@ -112,7 +281,12 @@ namespace metrisca {
             for (auto [steps, matrix] : result.Value()) {
                 
                 for (size_t key = 0; key != 256; key++) {
-                    double score = 0.0; ///TODO: Do the score thingy
+                    double score = -DOUBLE_INFINITY; 
+
+                    // Find the sample that "leaks" the most, this is the score
+                    for (size_t sample = 0; sample != matrix.GetWidth(); ++sample) {
+                        score = std::max(matrix(key, sample), score);
+                    }
 
                     scoresPerSteps[steps][keyByteIdx][key] = score;
                 }
@@ -122,5 +296,4 @@ namespace metrisca {
 
         return scoresPerSteps;
     }
-
 }
